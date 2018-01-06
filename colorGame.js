c8881bfce9e2a25a1cf85cae72bd7752d48fdaff
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-01-05 23:10:26 -0500

    Finish part 3 Randomize list and change color
    Randomized the selecting of picked colors, and add a changeColor() function. It changes all the colors to the correct color

--- colorGame.js
+++ colorGame.js
@@ -9,8 +9,9 @@ var colors = [
 
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,9 +25,26 @@ for(var i = 0; i < squares.length; i++) {
 
     //compare to pickedColor
     if (clickedColor === pickedColor) {
-      alert("correct");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("wrong");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again!";
     }
   });
+}
+
+
+function changeColors(color) {
+  //loop through all squares
+  for (var i = 0; i < colors.length; i++) {
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
