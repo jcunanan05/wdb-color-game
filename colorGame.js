52c664972d5e98e13df671f02cb1be0d8626c0ce
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-01-10 00:29:13 -0500

    Finish part 7
    Working on some styling and transitions

--- colorGame.js
+++ colorGame.js
@@ -57,7 +57,7 @@ resetButton.addEventListener("click", function() {
     squares[i].style.backgroundColor = colors[i];
   }
 
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 for(var i = 0; i < squares.length; i++) {
